ddb99bfdfd8552f4c18525f89e66330d50c91ca8
Trent Clarke <trent.clarke@gmail.com>
2016-03-17 22:37:06 +1100

    Experimenting with router

--- src/main.rs
+++ src/main.rs
@@ -1,6 +1,19 @@
 extern crate iron;
+#[macro_use(router)] extern crate router;
+
 use iron::prelude::*;
+use router::{Router};
+
+
+fn handler(req: &mut Request) -> IronResult<Response> {
+    let router = req.extensions.get::<Router>().unwrap();
+    let name = router.find("name").unwrap_or("<anonymous>");
+    let resp = Response::with((iron::status::Ok, "Hello, ".to_owned() + name));
+    Ok(resp)
+}
 
 fn main() {
-    println!("Hello, world!");
+    let router = router!(get "/" => handler,
+                         get "/:name" => handler);
+    Iron::new(router).http("localhost:2345").unwrap();
 }
